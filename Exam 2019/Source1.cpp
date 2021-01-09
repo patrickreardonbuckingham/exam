c8bccdb7401d20c3f63924f715c10416a63c9d4e
Patrick Reardon <75320203+patrickreardonbuckingham@users.noreply.github.com>
2021-01-09 11:13:21 +0000

    Add files via upload

---
+++ Exam 2019/Source1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	int nummonths;
+
+	double monthlyrate, totalservices;
+
+	cout << "Enter the monthly rate: " << ends;
+
+	cin >> monthlyrate;  // Customer enters the monthly rate
+
+	system("pause");
+
+	return 0;
+
+}
